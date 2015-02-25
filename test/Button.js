ece8274bd027e0b96be291eacf2b2a6c3b37e51f
Contra <contra@maricopa.edu>
2015-02-25 11:46:29 -0800

    fix tests

--- test/Button.js
+++ test/Button.js
@@ -7,14 +7,14 @@ var Button = React.createFactory(require('../lib/Button'));
 describe('Button()', function() {
   it('should render with no options', function(done){
     var btn = Button();
-    var str = React.renderToString(btn);
+    var str = React.renderToStaticMarkup(btn);
     should.exist(str);
     str.should.equal('<div class="ui button"></div>');
     done();
   });
   it('should render with children', function(done){
     var btn = Button(null, 'Test');
-    var str = React.renderToString(btn);
+    var str = React.renderToStaticMarkup(btn);
     should.exist(str);
     str.should.equal('<div class="ui button">Test</div>');
     done();
